7789bfb1c71fea9bd3d5399aa77622ebd9c419b8
Ramsey Meow <143419112+RamseyMeow2@users.noreply.github.com>
2023-11-27 03:50:01 -0500

    Fixed Weather API

--- js/temperature.js
+++ js/temperature.js
@@ -1,6 +1,15 @@
-var inputval = document.querySelector('#stateInput')
+var inputval = document.querySelector('#cityinput')
 var btn = document.querySelector('#add');
-var locations = document.querySelector('#locations')
+var temp = document.querySelector('#temp')
+
+
+apik = "3045dd712ffe6e702e3245525ac7fa38"
+
+
+function convertion(val) {
+    return (val - 273).toFixed(2)
+}
+
 
 btn.addEventListener('click', function () {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputval.value + '&appid=' + apik)
